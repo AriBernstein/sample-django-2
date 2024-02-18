014ca11f98dbf1b60a2d74fa3f14553369c0c0fe
Ari Bernstein <aribernstein1997@gmail.com>
2024-02-18 16:31:00 -0500

    use Docker to setup testing environment

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 Following the official Django First App tutorial
 
 
+access Docker file system for Django app
+```
+docker exec -t -i django /bin/bash
+```
+
+update after change to model
+```
+docker exec -t -i django /bin/bash -c "python3 manage.py makemigrations ; python3 manage.py migrate"
+```
+
 python3.10 -m venv venv
 . venv/bin/activate
 pip3 install django
 django-admin startproject pollsproject
 cd pollsproject 
-python3 manage.py startapp demoapp
+python3 manage.py startapp pollsapp
 python3 manage.py runserver
--- pollsproject/pollsproject/settings.py
+++ pollsproject/pollsproject/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -25,7 +28,7 @@ SECRET_KEY = "django-insecure-9+flv9ku0j6v!#7)^*0dv#&yf-)0e1@v-*bp#@sr9gw-5y^b1q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "pollsapp",
 ]
 
 MIDDLEWARE = [
@@ -76,9 +80,11 @@ WSGI_APPLICATION = "pollsproject.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "",
-        "USER": "",
-        "PASSWORD": "",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": 'db',
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
 
